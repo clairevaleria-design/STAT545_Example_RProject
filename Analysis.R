0a1a8d2bc67bc8867b9880686f65f97dc719f1b7
clairevaleria-design <clairevaleria@hotmail.com>
2025-09-11 10:08:19 -0700

    adding READMEand plot analysis

--- Analysis.R
+++ Analysis.R
@@ -1,2 +1,8 @@
-#this is a comment, calculate mean mean mpg from mtcars dataset
+# view the first few rows of mtcars dataset
+head(mtcars)
+
+# calculate the average mpg among all cars, save it as a variable
 mean_mpg <- mean(mtcars$mpg)
+
+# print the mean mpg
+mean_mpg
